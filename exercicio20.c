b43a5ce88121d86d18b06d7334254f3b312b2423
Venilton FalvoJr <falvojr@gmail.com>
2017-04-06 07:51:44 -0300

    Add files via upload

--- exercicio20.c
+++ exercicio20.c
@@ -17,23 +17,27 @@ int main() {
 
     printf("Digite a quantidade de lados: ");
     scanf("%d", &lados);
-    printf("Digite a medida dos lados: ");
-    scanf("%f", &medidaLados);
 
-    if(lados == 3) {
-        printf("TRIANGULO\n");
-        float perimetro = medidaLados * 3;
-        printf("Perimetro %.2f", perimetro);
-    }
-    else if(lados == 4) {
-        printf("QUADRADO\n");
-        float area = medidaLados * medidaLados;
-        printf("Area %.2f", area);
-    }
-    else if(lados == 5) {
-        printf("PENTAGONO");
-    }
-    else {
-        printf("INVALIDO!");
+    if(lados < 3) {
+        printf("NAO E UM POLIGONO");
+    } else if(lados > 5) {
+        printf("POLIGONO NAO IDENTIFICADO");
+    } else {
+        printf("Digite a medida dos lados: ");
+        scanf("%f", &medidaLados);
+
+        if(lados == 3) {
+            printf("TRIANGULO\n");
+            float perimetro = medidaLados * 3;
+            printf("Perimetro %.2f", perimetro);
+        }
+        else if(lados == 4) {
+            printf("QUADRADO\n");
+            float area = medidaLados * medidaLados;
+            printf("Area %.2f", area);
+        }
+        else if(lados == 5) {
+            printf("PENTAGONO");
+        }
     }
 }
